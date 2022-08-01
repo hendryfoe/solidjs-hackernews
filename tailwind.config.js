4b610e8602be0be6d807af1b670e0a9ee516caab
hendry-foe <hendryanto.fudiko@gmail.com>
2022-08-01 22:28:41 +0700

    feat: adjust style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
+    fontSize: {
+      lg: ['1.125rem', { lineHeight: '1.5rem' }] // changed from 1.75rem
+    },
     extend: {
       backgroundColor: {
         'hackernews-title': '#ff6600',
