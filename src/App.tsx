956d1e9cf4e03c15f548d87b74dbbed53e5b2629
hendry-foe <hendryanto.fudiko@gmail.com>
2022-08-01 22:31:35 +0700

    feat: add more functionality
    - implement "more page"
    - adjust style
    - add validation

--- src/App.tsx
+++ src/App.tsx
@@ -14,76 +14,10 @@ const Header: Component = () => {
   );
 };
 
-interface ItemProps {
-  no: string;
-  title: string;
-  source: string;
-  sourceDomain: string;
-  points: number;
-  author: string;
-  createdAt: string;
-  commentsCount: number;
-}
-const Item: FlowComponent<ItemProps, (isOpen: boolean) => JSXElement> = (props) => {
-  const [isOpen, setIsOpen] = createSignal(false);
-
-  function openCommentSection() {
-    setIsOpen(!isOpen());
-  }
-
-  return (
-    <section class="p-1 flex flex-col">
-      <div class="px-1 flex text-gray-400 gap-2">
-        <div class="text-2xl min-w-[2rem] text-right">{props.no}</div>
-        <div class="flex flex-col flex-1">
-          <div class="flex flex-wrap items-baseline">
-            <a href={props.source}>
-              <h2 class="text-2xl text-black mr-1">{props.title}</h2>
-            </a>
-            {/* <span>
-              (
-              <a href="" class="hover:underline">
-                {props.sourceDomain}
-              </a>
-              )
-            </span> */}
-          </div>
-
-          <div>
-            <span>
-              {props.points} points by {props.author} {props.createdAt} |{' '}
-              <button class="hover:underline" onClick={openCommentSection}>
-                {props.commentsCount} comments
-              </button>
-              {/* 40 points by Hbruz0 2 hours ago | hide | 22 comments */}
-            </span>
-          </div>
-          <Show when={isOpen()}>
-            <div class="overflow-scroll max-h-96 bg-white py-2 px-5">{props.children(isOpen())}</div>
-          </Show>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-interface CommentProps {
-  data: CommentData;
-}
-const Comment: VoidComponent<CommentProps> = (props) => {
-  return (
-    <section class="py-1 mb-2">
-      <h5 class="text-gray-500">
-        {props.data.by} {getDifferentInDays(new Date(props.data.time * 1000), new Date())}
-      </h5>
-      <div class="text-black text-lg pt-1 [&_a]:underline [&_a]:text-gray-500 [&_p]:mt-2" innerHTML={props.data.text} />
-    </section>
-  );
-};
-
 export interface CommentData {
   by: string;
   id: number;
+  dead?: boolean;
   kids?: number[];
   parent: number;
   text: string;
@@ -91,10 +25,26 @@ export interface CommentData {
   type: string;
 }
 
+interface CommentProps {
+  data: CommentData;
+}
+const Comment: VoidComponent<CommentProps> = (props) => {
+  return (
+    <section class="py-1 mb-2">
+      <h5 class="text-gray-500 text-sm">
+        {props.data.by} {getDifferentInDays(new Date(props.data.time * 1000), new Date())}
+      </h5>
+      <div class="text-black text-lg pt-1 [&_a]:underline [&_a]:text-gray-500 [&_p]:mt-2" innerHTML={props.data.text} />
+    </section>
+  );
+};
+
 async function fetchComments(ids: number[]): Promise<CommentData[]> {
   const endpoint = `${EndpointConstant.API_URL}${EndpointConstant.ITEM}`;
   const endpointPromises = ids.map((id) => Request.get<CommentData>(`${endpoint}/${id}.json`));
-  return Promise.all(endpointPromises);
+  return Promise.all(endpointPromises).then((responses) => {
+    return responses.filter((response) => !response.dead);
+  });
 }
 
 interface CommentsProps {
@@ -109,22 +59,20 @@ const Comments: Component<CommentsProps> = (props) => {
       <Show when={comments()} fallback={<div>Loading...</div>}>
         {(comments) => {
           return (
-            <div class="flex flex-col">
-              <For each={comments}>
-                {(comment) => (
-                  <>
-                    <Comment data={comment} />
-                    <Show when={comment.kids}>
-                      {(kids) => (
-                        <div class="pl-10">
-                          <Comments isOpen={props.isOpen} itemIds={kids} />
-                        </div>
-                      )}
-                    </Show>
-                  </>
-                )}
-              </For>
-            </div>
+            <For each={comments}>
+              {(comment) => (
+                <>
+                  <Comment data={comment} />
+                  <Show when={comment.kids}>
+                    {(kids) => (
+                      <section class="pl-4 sm:pl-6 md:pl-8 lg:pl-10">
+                        <Comments isOpen={props.isOpen} itemIds={kids} />
+                      </section>
+                    )}
+                  </Show>
+                </>
+              )}
+            </For>
           );
         }}
       </Show>
@@ -144,11 +92,6 @@ interface ItemData {
   url: string;
 }
 
-function storiesFetcher() {
-  const endpoint = `${EndpointConstant.API_URL}${EndpointConstant.STORIES}`;
-  return Request.get<number[]>(endpoint);
-}
-
 interface ItemFetcherResponse {
   data: ItemData[];
   canNext: boolean;
@@ -156,16 +99,70 @@ interface ItemFetcherResponse {
 
 async function itemFetcher(stories: number[], cursor: number): Promise<ItemFetcherResponse> {
   const endpoint = `${EndpointConstant.API_URL}${EndpointConstant.ITEM}`;
-  const start = cursor;
-  const end = GeneralConstant.ITEMS_PER_PAGE;
+  const start = cursor * GeneralConstant.ITEMS_PER_PAGE;
+  const end = (cursor + 1) * GeneralConstant.ITEMS_PER_PAGE;
   const endpointPromises = stories.slice(start, end).map((id) => {
     return Request.get<ItemData>(`${endpoint}/${id}.json`);
   });
 
   const data = await Promise.all(endpointPromises);
-  const canNext = stories.length < (cursor + 1) * GeneralConstant.ITEMS_PER_PAGE;
+  const canNext = stories.length > (cursor + 1) * GeneralConstant.ITEMS_PER_PAGE;
 
   return { data, canNext };
+}
+interface ItemProps {
+  no: string;
+  title: string;
+  source: string;
+  sourceDomain: string;
+  points: number;
+  author: string;
+  createdAt: string;
+  commentsCount: number;
+}
+const Item: FlowComponent<ItemProps, (isOpen: boolean) => JSXElement> = (props) => {
+  const [isOpen, setIsOpen] = createSignal(false);
+
+  function handleOpenCommentSection() {
+    setIsOpen(!isOpen());
+  }
+
+  return (
+    <section class="py-2 flex text-gray-400 gap-2">
+      <div class="text-xl min-w-[2rem] text-right">{props.no}</div>
+      <div class="w-[calc(100%-2.5rem)]">
+        <div class="flex flex-wrap items-baseline">
+          <a href={props.source}>
+            <h2 class="text-lg text-black mr-1">{props.title}</h2>
+          </a>
+          {/* <span>
+            (
+            <a href="" class="hover:underline">
+              {props.sourceDomain}
+            </a>
+            )
+          </span> */}
+        </div>
+
+        <div class="text-sm">
+          {props.points} points by {props.author} {props.createdAt} |{' '}
+          <button class="hover:underline" onClick={handleOpenCommentSection} disabled={props.points <= 0}>
+            {props.commentsCount} comments
+          </button>
+        </div>
+        <Show when={isOpen()}>
+          <div class="overflow-scroll max-h-96 bg-white py-2 px-5 w-[calc(100%-10px)] max-w-[calc(100%-10px)]">
+            {props.children(isOpen())}
+          </div>
+        </Show>
+      </div>
+    </section>
+  );
+};
+
+function storiesFetcher() {
+  const endpoint = `${EndpointConstant.API_URL}${EndpointConstant.STORIES}`;
+  return Request.get<number[]>(endpoint);
 }
 
 const App: Component = () => {
@@ -174,20 +171,32 @@ const App: Component = () => {
   const [items, { mutate, refetch }] = createResource<ItemFetcherResponse, number[]>(
     () => stories(),
     (stories, { value, refetching }) => {
-      return itemFetcher(stories, cursor());
+      const results = itemFetcher(stories, cursor());
+
+      if (refetching && refetching === 'next' && value?.canNext) {
+        return results.then(({ data, canNext }) => ({
+          data: [...value.data, ...data],
+          canNext
+        }));
+      }
+      return results;
     }
   );
 
+  function handleFetchMore() {
+    setCursor(cursor() + 1);
+    refetch('next');
+  }
+
   return (
-    <div class="w-full bg-hackernews-body">
-      {/* <div class="w-full sm:w-3/4 md:w-2/4 lg:w-1/4 m-auto bg-hackernews-body"> */}
+    <div class="w-full bg-hackernews-body md:container md:my-2 m-auto">
       <Header />
       <Show when={stories.error}>Something went wrong. {stories.error}</Show>
       <Show when={items.error}>Something went wrong. {items.error}</Show>
       <Show when={items()} fallback={<div>Loading....</div>}>
         {(items) => {
           return (
-            <>
+            <section class="py-1 pl-1 pr-4 flex flex-col">
               <For each={items.data}>
                 {(item, index) => {
                   return (
@@ -206,10 +215,12 @@ const App: Component = () => {
                   );
                 }}
               </For>
-              <div class="text-gray-400 text-2xl mt-3 ml-12 pb-10">
-                <a href="">More</a>
-              </div>
-            </>
+              <Show when={items.canNext}>
+                <div class="text-gray-400 text-2xl mt-3 ml-10 pb-10">
+                  <button onClick={handleFetchMore}>More</button>
+                </div>
+              </Show>
+            </section>
           );
         }}
       </Show>
